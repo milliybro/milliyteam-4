36b20caa4b7c8b19876b24a8a962dc82c92a1255
“milliybro” <shohrux-rustamov@mail.ru>
2023-08-03 22:53:32 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./**/*.{html,js}","./src/**/*.{html,js}", "./pages/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "600px",
+      md: "800px",
+      lg: "1000px",
+      xl: "1250px",
+      xxl: "1500px",
+    },
+    colors: {
+      primary: "#FFC000",
+      secondary: "#F2F3F7",
+      white: "white",
+      grey: "grey",
+      blue: "blue",
+      black: "black",
+      btn: "rgba(0, 0, 0, 0.05)",
+      bg_pr: "#FAFAFA",
+      bg_second: "rgba(245, 138, 7, 0.10)",
+      bl_gradient: "#0C0926",
+      input: "#DBDEE7,"
+    },
+    container: {
+      padding: "20px",
+      maxWidth: {
+        sm: "500px",
+        md: "700px",
+        lg: "900px",
+        xl: "1100px",
+        xxl: "1300px",
+      },
+    },
+  },
+  plugins: [],
+};
